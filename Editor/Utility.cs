f12df04ab7388f8e9a6e49d74c9a82fe353eb387
AlianBlank <wangfj11@foxmail.com>
2024-03-24 16:24:42 +0800

    [增加]1. 增加类型的映射

--- Editor/Utility.cs
+++ Editor/Utility.cs
@@ -13,43 +13,48 @@ namespace Proto2CS.Editor
             {
                 case "int16":
                     typeCs = "short";
-
-                    break;
-                case "int32":
-                    typeCs = "int";
-
-                    break;
-                case "bytes":
-                    typeCs = "byte[]";
-
-                    break;
-                case "uint32":
-                    typeCs = "uint";
-
-                    break;
-                case "long":
-                    typeCs = "long";
-
-                    break;
-                case "int64":
-                    typeCs = "long";
-
-                    break;
-                case "uint64":
-                    typeCs = "ulong";
-
                     break;
                 case "uint16":
                     typeCs = "ushort";
-
                     break;
-                case "map<int32,long>":
+                case "int32":
+                case "sint32":
+                case "sfixed32":
+                    typeCs = "int";
+                    break;
+                case "uint32":
+                case "fixed32":
+                    typeCs = "uint";
+                    break;
+                case "int64":
+                case "sint64":
+                case "sfixed64":
+                    typeCs = "long";
+                    break;
+                case "uint64":
+                case "fixed64":
+                    typeCs = "ulong";
+                    break;
+                case "bytes":
+                    typeCs = "byte[]";
+                    break;
+                case "map<int32,int64>":
                     typeCs = "Dictionary<int, long>";
-
+                    break;
+                case "string":
+                    typeCs = "string";
+                    break;
+                case "bool":
+                    typeCs = "bool";
+                    break;
+                case "double":
+                    typeCs = "double";
+                    break;
+                case "float":
+                    typeCs = "float";
                     break;
                 default:
                     typeCs = type;
-
                     break;
             }
 
